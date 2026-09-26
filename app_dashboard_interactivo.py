ac379ade25eca35d7377620f5dfd989f2b1886c1
generoxperu-dotcom <generoxperu@gmail.com>
2026-09-26 10:36:05 -0500

    Update app_dashboard_interactivo.py

--- app_dashboard_interactivo.py
+++ app_dashboard_interactivo.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 
 # --- 1. CONFIGURACIÓN VISUAL GENERAL ---
 st.set_page_config(
-    page_title="Management Dashboard | GlobalTech",
+    page_title="Dashboard de Ventas",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded"
